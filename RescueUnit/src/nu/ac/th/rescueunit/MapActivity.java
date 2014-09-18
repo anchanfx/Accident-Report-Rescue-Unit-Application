c5c7a800803d346bc4388985073c5c7984cc7286
watcharakit <gun_1288@hotmail.com>
2014-09-19 00:33:29 +0700

    insert marker on google map

--- RescueUnit/src/nu/ac/th/rescueunit/MapActivity.java
+++ RescueUnit/src/nu/ac/th/rescueunit/MapActivity.java
@@ -1,12 +1,36 @@
 package nu.ac.th.rescueunit;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MapActivity extends Activity{
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.map);
-	}
+	static final LatLng KIEL = new LatLng(16.742906328, 100.203977484);
+	  private GoogleMap map;
+
+	  @SuppressLint("NewApi")
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.map);
+	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment1))
+	        .getMap();
+	    
+	    if (map!=null){
+	      Marker kiel = map.addMarker(new MarkerOptions()
+	      	  .position(KIEL)
+	          .title("Kiel")
+	          .snippet("Kiel is cool")
+	          .icon(BitmapDescriptorFactory
+	          .fromResource(R.drawable.ic_launcher)));
+	    }
+	    
+	  } 
 }
