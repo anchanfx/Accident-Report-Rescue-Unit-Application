be43de3bbe591f0bed05843abbd43563d516c910
watcharakit <gun_1288@hotmail.com>
2014-10-14 00:03:41 +0700

    เขียน function selfUpdateThread เพิ่ม

--- RescueUnit/src/nu/ac/th/rescueunit/SelfUpdateThread.java
+++ RescueUnit/src/nu/ac/th/rescueunit/SelfUpdateThread.java
@@ -2,8 +2,16 @@ package nu.ac.th.rescueunit;
 
 public class SelfUpdateThread implements Runnable{
 	private SelfUpdateThreadListener listener;
+	private IServerConnector connector;
+	
+	public SelfUpdateThread(SelfUpdateThreadListener listener,IServerConnector connector){
+		super();
+		this.listener = listener;
+		this.connector = connector;
+	}
 	
 	public void run(){
+		
 		
 	}
 }
